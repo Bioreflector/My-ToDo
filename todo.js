ab68303e60972b6354dc7d21f054015c7cc155a3
Bioreflector <Bioreflector@gmail.com>
2022-09-14 17:15:51 +0300

    create add todo

--- todo.js
+++ todo.js
@@ -1,10 +1,47 @@
 const formToDo = document.formToDo
-console.log(formToDo)
-const {addInput,searchInput ,addСaseBtn ,searchСaseBtn} = formToDo
+const todolist =document.querySelector('.todo-list')
+const {addInput,searchInput ,addTodoBtn ,searchTodoBtn} = formToDo
+const defaultUrl = "http://localhost:3000/todolist"
 
-addСaseBtn.addEventListener('click' , addCase)
+addTodoBtn.addEventListener('click' , addTodo)
 
-function addCase (event){
+    async function addTodo(event){
     event.preventDefault()
-    console.log("addCase")
+    const text = addInput.value
+    const responce = await fetch(defaultUrl,
+        {
+        method: "POST",
+        headers: {
+            Accept:'application/json',
+            'Content-Type':'application/json',
+           
+        },
+        body: JSON.stringify({text})
+        }
+    )
+    todolist.innerHTML = ""
+    if(responce.ok){
+        rander()
+    }  
 }
+function deleteTodo(){
+    console.log("delete")
+}
+
+function createTodolist(item){
+    const li = document.createElement("li")
+    const deleteButton = document.createElement("button")
+    deleteButton.innerText = "Delete"
+    console.log(deleteButton)
+    li.innerText = item.text
+    li.insertAdjacentElement("afterend" , deleteButton)
+    return li
+}
+
+async function rander(){
+    const response = await fetch(defaultUrl)
+    const arrTodo = await response.json()
+    const list = arrTodo.map(item => createTodolist(item))
+    list.forEach(item => todolist.insertAdjacentElement("beforeend" , item))
+}
+
